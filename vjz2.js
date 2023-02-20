9c5a32dc39cf721c65861e71c8368761906ea7df
Ante Sesar <ante.sesar11@gmail.com>
2023-02-20 19:25:24 +0100

    end

---
+++ vjz2.js
@@ -0,0 +1,30 @@
+/*var firstName = "Ante";
+var lastName = "Sesar";
+var fullName = `${firstName} ${lastName}`;
+//var fullName = firstName + " " + lastName;
+
+console.log("full name: " + fullName)
+//console.log(fullName);
+*/
+
+/*var z = 2;
+var y = 17;
+var x = z === 2 ? y : 5;
+
+if (z === 2) {
+    x = y;
+} else {
+    x = 5;
+}
+
+console.log("x = " + x)*/
+
+
+for (var i = 1; i <= 20; i++) {
+    if (i % 2 == 0) {
+        console.log("Broj " + i + " je paran");
+    }
+    else {
+        console.log("Broj " + i + " je neparan");
+    }
+}
---
+++ vjz3.js
@@ -0,0 +1,9 @@
+var names = ["John", "Jane",, "Mike"];
+
+for(let i=0; i<=names.length; ++i){
+    console.log(names[i])
+}
+
+names.forEach(function(value){
+    console.log(value);
+});
